2bc273b0fce13928417f907af485b4ffc9dbecb5
Noah Cowan <nsncowan@gmail.com>
2022-12-21 19:04:25 -0800

    pass test: create calculator object.

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,9 @@
 export default class Calculator {
-  
+  constructor() {
+    this.earthYears = 1;
+    this.mercuryYears = 0.24;
+    this.venusYears = 0.62;
+    this.marsYears = 1.88;
+    this.jupiterYears = 11.86;
+  }
 }
