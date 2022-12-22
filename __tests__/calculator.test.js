d9b02ca9c002d754bd9086110d64f0ba65f05e3c
Noah Cowan <nsncowan@gmail.com>
2022-12-21 20:42:26 -0800

    new test: convert input age into mars years.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -6,22 +6,26 @@ describe('Calculator', () => {
   test('should correctly create a calculator object with year equivalences', () => {
     const calculator = new Calculator(35);
     expect(calculator.earthYears).toEqual(35);
-    expect(calculator.mercuryYears).toEqual(35/0.24);
-    expect(calculator.venusYears).toEqual(35/0.62);
-    expect(calculator.marsYears).toEqual(35/1.88);
-    expect(calculator.jupiterYears).toEqual(35/11.86);
+    expect(calculator.mercuryYears).toEqual(0.24);
+    expect(calculator.venusYears).toEqual(0.62);
+    expect(calculator.marsYears).toEqual(1.88);
+    expect(calculator.jupiterYears).toEqual(11.86);
   });
 
-  test('should return an age of 35 earth years to mercury (35/.0.24)', () => {
+  test('should convert an age of 35 earth years to mercury (35 / 0.24)', () => {
     const newCalc = new Calculator(35);
-    expect(newCalc.giveMercuryAge()).toEqual(35/0.24);
+    expect(newCalc.giveMercuryAge()).toEqual(35 / 0.24);
   })
 
-  test('should return an age of 35 earth years to venus (35/.0.62)', () => {
+  test('should convert an age of 35 earth years to venus (35 / 0.62)', () => {
     const newCalc = new Calculator(35);
-    expect(newCalc.giveVenusAge()).toEqual(35/0.62);
+    expect(newCalc.giveVenusAge()).toEqual(35 / 0.62);
   })
 
+  test('should convert an age of 35 earth years to mars (35 / 1.88)', () => {
+    const newCalc = new Calculator(35);
+    expect(newCalc.giveMarsAge()).toEqual(35 / 1.88);
+  })
 
 
 
--- src/calculator.js
+++ src/calculator.js
@@ -1,18 +1,18 @@
 export default class Calculator {
   constructor(age) {
     this.earthYears = age;
-    this.mercuryYears = age/0.24;
-    this.venusYears = age/0.62;
-    this.marsYears = age/1.88;
-    this.jupiterYears = age/11.86;
+    this.mercuryYears = 0.24;
+    this.venusYears = 0.62;
+    this.marsYears = 1.88;
+    this.jupiterYears = 11.86;
   }
 
   giveMercuryAge() {
-    return this.mercuryYears;
+    return this.earthYears / this.mercuryYears;
   }
  
   giveVenusAge() {
-    return this.venusYears;
+    return this.earthYears / this.venusYears;
   }
 
 
