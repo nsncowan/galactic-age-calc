2a3f690304a4490f166971cc483a10bf5e8e3d32
Noah Cowan <nsncowan@gmail.com>
2022-12-22 14:29:38 -0800

    pass 6th test: return array of years passed on each planet since a chosen birthday.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -4,12 +4,14 @@ import Calculator from "../src/calculator";
 describe('Calculator', () => {
 
   test('should correctly create a calculator object with year equivalences', () => {
-    const calculator = new Calculator(35);
-    expect(calculator.earthYears).toEqual(35);
+    const calculator = new Calculator(35, 25, 65);
+    expect(calculator.earthYears).toEqual(1);
     expect(calculator.mercuryYears).toEqual(0.24);
     expect(calculator.venusYears).toEqual(0.62);
     expect(calculator.marsYears).toEqual(1.88);
     expect(calculator.jupiterYears).toEqual(11.86);
+    expect(calculator.pastBirthday).toEqual(25);
+    expect(calculator.futureBirthday).toEqual(65);
   });
 
   test('should convert an age of 35 earth years to mercury years (35 / 0.24)', () => {
@@ -34,7 +36,7 @@ describe('Calculator', () => {
 
   test('should return an array of planetary years for each planet since past birthday', () => {
     const newCalc = new Calculator(35, 25, 60);
-    expect(newCalc.yearsPast()).toEqual([(35-25)/0.24, (35-25)/0.62, (35-25)/1.88, (35-25)/11.86]);
+    expect(newCalc.yearsPast()).toEqual([(35-25)/1, (35-25)/0.24, (35-25)/0.62, (35-25)/1.88, (35-25)/11.86]);
   });
 
 });
--- src/calculator.js
+++ src/calculator.js
@@ -1,29 +1,36 @@
 export default class Calculator {
   constructor(age, pastBirthday, futureBirthday) {
-    this.earthYears = age;
+    this.earthYears = 1;
     this.mercuryYears = 0.24;
     this.venusYears = 0.62;
     this.marsYears = 1.88;
     this.jupiterYears = 11.86;
+    this.age = age;
     this.pastBirthday = pastBirthday;
     this.futureBirthday = futureBirthday;
   }
 
   giveMercuryAge() {
-    return this.earthYears / this.mercuryYears;
+    return this.age / this.mercuryYears;
   }
  
   giveVenusAge() {
-    return this.earthYears / this.venusYears;
+    return this.age / this.venusYears;
   }
 
   giveMarsAge() {
-    return this.earthYears / this.marsYears;
+    return this.age / this.marsYears;
   }
 
   giveJupiterAge() {
-    return this.earthYears / this.jupiterYears;
+    return this.age / this.jupiterYears;
   }
 
+  yearsPast() {
+    const yearsPastGap = this.age - this.pastBirthday;
+    const yearsPastArray = [yearsPastGap/this.earthYears, yearsPastGap/this.mercuryYears, yearsPastGap/this.venusYears, yearsPastGap/this.marsYears, yearsPastGap/this.jupiterYears];
+
+    return yearsPastArray;
+  }
 
 }
